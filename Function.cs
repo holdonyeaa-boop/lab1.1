2ec878e68272b7c5328fc2666149fd1362f63097
rcgmd <holdonyeaa@gmail.com>
2025-10-17 12:32:26 +0300

    Update Function.cs
    Изменение в функции

--- Function.cs
+++ Function.cs
@@ -8,7 +8,7 @@ namespace MyApplication
         {
             if (11.0 * x <= 0.0)
                 throw new ArgumentException($"Значение x = {x} не принадлежит области определения функции (требуется 11*x > 0).");
-            return 11.0 * x - Math.Log(11.0 * x) - 2.0;
+            return 11.0 * x - Math.Log(11.0 * x) - 1.0;
         }
     }
 }
